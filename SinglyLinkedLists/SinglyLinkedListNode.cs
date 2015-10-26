0f88caa9a9544bbe99e6e529f8ed21e81b957e57
Rick8s <rick8s@hotmail.com>
2015-10-26 18:28:55 -0500

    First commit with three tests passing

--- SinglyLinkedLists/SinglyLinkedListNode.cs
+++ SinglyLinkedLists/SinglyLinkedListNode.cs
@@ -15,14 +15,15 @@ namespace SinglyLinkedLists
         private SinglyLinkedListNode next;
         public SinglyLinkedListNode Next
         {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+       
+            get { return next; }
+            set {next = value; } 
         }
-
+    
         private string value;
         public string Value 
         {
-            get { throw new NotImplementedException(); }
+            get { return value; }
         }
 
         public static bool operator <(SinglyLinkedListNode node1, SinglyLinkedListNode node2)
@@ -39,8 +40,7 @@ namespace SinglyLinkedLists
 
         public SinglyLinkedListNode(string value)
         {
-            throw new NotImplementedException();
-
+            this.value = value;
             // Used by the visualizer:
             allNodes.Add(this);
         }
